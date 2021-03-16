70f8669baacffda8adc3233a51a6c5b507114d56
vileninja015 <vileninja@outlook.com>
2021-03-16 17:47:59 -0400

    Update mycog.py

--- MyCog/mycog.py
+++ MyCog/mycog.py
@@ -1,10 +1,15 @@
 from redbot.core import commands
+import Discord
+import asyncio
 
 class Mycog(commands.Cog):
     """My custom cog"""
 
-    @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("<@&801253540534157332> I can do stuff!")
+    @client.event
+async def on_message(message):
+  text_channel = client.get_channel('806000602168819723')
+  msg = '{0.author.mention}\nWrong text channel\nUse {1.mention}'.format(message,text_channel)
+  if message.content.startswith('.ann'):
+      await client.delete_message(message)
+      await client.send_message(message.channel, msg)
+  return
