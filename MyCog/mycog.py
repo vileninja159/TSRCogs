d48a9536892817704668ed47bdf60e5f8f4940df
vileninja015 <vileninja@outlook.com>
2021-03-16 17:52:35 -0400

    Update mycog.py

--- MyCog/mycog.py
+++ MyCog/mycog.py
@@ -4,7 +4,9 @@ import asyncio
 
 class Mycog(commands.Cog):
     """My custom cog"""
-        
+      
+      
+      @commands.command()
 async def mycom(message):
   text_channel = client.get_channel('806000602168819723')
   msg = '{0.author.mention}\nWrong text channel\nUse {1.mention}'.format(message,text_channel)
