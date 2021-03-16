99ed393341682ce20009cae90b28c252b7b5a2c7
vileninja015 <vileninja@outlook.com>
2021-03-16 17:49:13 -0400

    Update mycog.py

--- MyCog/mycog.py
+++ MyCog/mycog.py
@@ -4,10 +4,10 @@ import asyncio
 
 class Mycog(commands.Cog):
     """My custom cog"""
-
-    @client.event
-async def on_message(message):
-  text_channel = client.get_channel('806000602168819723')
+        
+      @client.event
+async def mycom(message):
+  text_channel = client.get_channel('1234567890')
   msg = '{0.author.mention}\nWrong text channel\nUse {1.mention}'.format(message,text_channel)
   if message.content.startswith('.ann'):
       await client.delete_message(message)
