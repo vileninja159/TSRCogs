94b140d1a83853878669890bca42bfe2bed3e9cb
vileninja015 <vileninja@outlook.com>
2021-03-16 18:26:45 -0400

    Update mycog.py

--- MyCog/mycog.py
+++ MyCog/mycog.py
@@ -7,4 +7,4 @@ class Mycog(commands.Cog):
     async def mycom(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("@admin I can do stuff!")
+        await ctx.send("<@&admin> I can do stuff!")
