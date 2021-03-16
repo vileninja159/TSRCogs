441fb022f644571409753de75a4e10348abd15f5
vileninja015 <vileninja@outlook.com>
2021-03-16 18:14:34 -0400

    Update mycog.py

--- MyCog/mycog.py
+++ MyCog/mycog.py
@@ -4,5 +4,7 @@ class Mycog(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-  async def mycom(self, ctx):
-     await ctx.send(ctx.message.guild.default_role)
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("@here I can do stuff!")
